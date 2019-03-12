6c02406b3220a5d8d0fa9f38c93cfbd0b9b8f14c
joseph871107 <joseph1231231234@gmail.com>
2019-03-12 17:39:56 +0800

    Optimizing the basic framework
    Chages:
    1.Add a new class named CGameComFunc in game_framework.
    2.In the CGameComFunc has a function OnShowText to simplify original method of showing text.
    3.Display current mouse coordinates and Keydown Keycode in the CGameStateRun.
    4.Rename mygame as brawlhalla.

--- Source/resource.h
+++ Source/resource.h
@@ -39,7 +39,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        159
+#define _APS_NEXT_RESOURCE_VALUE        162
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
